1115c416d0dfc9b2c905beca6b5bba769c277892
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-07 18:54:09 +0300

    add class Computer

--- src/com/artemdev/oop/lesson_02/Computer.java
+++ src/com/artemdev/oop/lesson_02/Computer.java
@@ -2,14 +2,32 @@ package com.artemdev.oop.lesson_02;
 
 public class Computer {
     int ssd = 500;
-    int ram = 1024;
+    long ram = 0;
+
+    public Computer() {
+        System.out.println("Я был создан!");
+    }
+
+    public Computer(int ssd) {
+        this.ssd = ssd;
+    }
+
+    public Computer(int ssd, long ram) {
+        this.ssd = ssd;
+        this.ram = ram;
+    }
 
     void load() {
         System.out.println("Я загрузился!");
     }
 
+    void load(boolean value) {
+        System.out.println("Я загрузился с одним параметром!");
+    }
+
     void printState() {
         System.out.println("SSD: " + ssd);
         System.out.println("Ram: " + ram);
+        System.out.println("------------");
     }
 }
--- src/com/artemdev/oop/lesson_02/ComputerRunner.java
+++ src/com/artemdev/oop/lesson_02/ComputerRunner.java
@@ -2,9 +2,15 @@ package com.artemdev.oop.lesson_02;
 
 public class ComputerRunner {
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        computer.load();
-        System.out.println("Ram: " + computer.ram);
-        System.out.println("SSD: " + computer.ssd);
+        Computer computer1 = new Computer();
+        computer1.load();
+        computer1.printState();
+
+        Computer computer2 = new Computer(1050);
+        computer2.printState();
+
+        Computer computer3 = new Computer(10000, 10000);
+        computer3.load(true);
+        computer3.printState();
     }
 }
